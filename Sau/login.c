bfc71a10dd25e7f206a95f7295d606001ee60a61
LucasToscanou <92105205+LucasToscanou@users.noreply.github.com>
2022-11-21 19:49:58 -0300

    Update login.c

--- Sau/login.c
+++ Sau/login.c
@@ -5,16 +5,6 @@
 #define TAMSE 15
 
 
-/* usa funcao de busca para encontrar se o login inserido existe dentro do arquivo,
-caso seja encontrado retorna 0
-caso nao seja encontrado retorna 1
-na main 1 enviara mensagem de erro "login ou senha invalido"
-0 entrara na aplicacao*/
-// recebe da busca 0 se existe e 1 se nao existe
-
-// manda arquivo ja aberto que contem todos os logins ja cadastrados 
-// para testar 
-
 int buscaArq(FILE *arq, char *usuario, char *senha)
 {
     int cmp; // pega retorno de strcmp 
