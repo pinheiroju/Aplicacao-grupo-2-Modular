3bdba6a1d1209192a6a67b233d67624b9598a5ec
LucasToscanou <92105205+LucasToscanou@users.noreply.github.com>
2022-11-21 19:49:29 -0300

    Update cadastro.h

--- Sau/cadastro.h
+++ Sau/cadastro.h
@@ -1,2 +1,9 @@
 int buscaArq(FILE *arq, char *usuario, char *senha);
+
+/** 
+ * @param {char*} Nome do arquivo com os cadastros.
+ * @param {char*} Matrícula inserida pelo usuário.
+ * @param {char*} Senha inserida pelo usuário.
+ * @returns {int} 1: para cadastro com sucesso; 2: se o par matrícula e senha ja esteja cadastrado; 0: caso já exista matrícula com outra senha.
+ */
 int cadastrar(FILE *arq, char *usuario, char *senha);
